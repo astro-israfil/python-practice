8543d1dc9e5b2032eeda0419d7ddc50b06b57fe6
astro-israfil <astro.israfil.fi@gmail.com>
2024-06-26 15:52:58 +0600

    learn conditional statement if else elif

---
+++ chapter_06/conditionals.py
@@ -0,0 +1,16 @@
+# age = int(input("Please enter your age: "))
+
+# if (age >= 18) :
+#     print("You are eligible to drive")
+#     have_license = input("Have you won driving license? (yes or no):  ")
+#     if (have_license == "yes") :
+#         print("Please drive the car")
+#     elif (have_license == "no") :
+#         print("Please don't drive")
+#     else :
+#         print("Invalid answer")
+# elif (age > 0) :
+#     print(f"You have to wait for {18 - age} years more to drive")
+# else :
+#     print(f"you entered an envalid age") 
+
